c49d3ac8d8801439a52aacdc1e1f835413ae3797
Mikolaj Gralczyk <mgralczyk@anaconda.com>
2025-09-29 14:59:40 +0200

    Convert to Anaconda's style

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,3 @@
 @ECHO ON
 set JUPYTER_PACKAGING_SKIP_NPM=1
-%PYTHON% -m pip install --no-deps -vv . || exit 1
+%PYTHON% -m pip install . -vv --no-deps --no-build-isolation || exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eux
 export JUPYTER_PACKAGING_SKIP_NPM=1
-${PYTHON} -m pip install --no-deps -vv .
+${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
