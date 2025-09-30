52cea9bdad2fc0324fc27ca1c0aa23476e5c3def
Mikolaj Gralczyk <mgralczyk@anaconda.com>
2025-09-30 09:25:35 +0200

    Simplify build without build scripts

--- recipe/bld.bat
+++
@@ -1,3 +0,0 @@
-@ECHO ON
-set JUPYTER_PACKAGING_SKIP_NPM=1
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation || exit 1
--- recipe/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-export JUPYTER_PACKAGING_SKIP_NPM=1
-${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
